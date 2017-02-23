584e2851d7e0b67dbbe853d09f1725b89576b30f
Helmut Burchardt <hburchardt@truelogic.software>
2017-02-23 14:39:02 -0300

    controller update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,5 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;//lalala
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;//lalaladssdas
 }
